0e057b13d6c0f855b7a1046d34e4254e6ca8f7a7
cxk <datehoer@gmail.com>
2024-01-13 14:45:30 +0800

    add bilibili, ithome

---
+++ ithome/ithome.py
@@ -0,0 +1,20 @@
+import requests
+import pyquery
+
+url = "https://m.ithome.com/rankm/"
+
+res = requests.get(url)
+
+doc = pyquery.PyQuery(res.content)
+data = []
+rank_day = doc("div[data-rank-type='day-rank']").next().find("a").items()
+for item in rank_day:
+    title = item.find("p.plc-title").text()
+    link = item.attr("href")
+    hotScore = 0
+    data.append({
+        "title": title,
+        "link": link,
+        "hotScore": hotScore
+    })
+print(data)
