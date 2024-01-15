815f4fb83a50132d8be99084869173ec3dda2591
datehoer <datehoer@gmail.com>
2024-01-15 09:20:44 +0800

    del toutiao.js, make toutiao perfect

--- toutiao/toutiao.py
+++ toutiao/toutiao.py
@@ -1,1 +1,21 @@
 #coding=utf-8
+import requests
+url = 'https://www.toutiao.com/hot-event/hot-board/?origin=toutiao_pc'
+
+
+res = requests.get(url)
+
+res_json = res.json()
+
+data = []
+
+for i in res_json.get('data', []):
+    title = i.get('Title', '')
+    link = i.get('Url', '')
+    hotScore = i.get('HotValue', '')
+    data.append({
+        'title': title,
+        'link': link,
+        'hotScore': hotScore
+    })
+print(data)
