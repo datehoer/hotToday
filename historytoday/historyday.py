7b821d72d26c162b5ed0a8a325a1ba87e9769a1a
datehoer <datehoer@gmail.com>
2024-11-06 21:15:06 +0800

    add history headers

--- historytoday/historyday.py
+++ historytoday/historyday.py
@@ -2,7 +2,24 @@ import requests
 import pyquery
 def get_history_today():
     url = "https://hao.360.com/histoday/"
-    response = requests.get(url)
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'cache-control': 'max-age=0',
+        'if-modified-since': 'Wed, 06 Nov 2024 13:12:01 GMT',
+        'priority': 'u=0, i',
+        'referer': 'https://www.google.com/',
+        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'cross-site',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+    }
+    response = requests.get(url, headers=headers)
     doc = pyquery.PyQuery(response.text)
     things = doc(".tih-list>dl>dt").items()
     data = []
