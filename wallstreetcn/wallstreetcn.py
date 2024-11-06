92fa10522e07e9b0e0d2a479de903f271d7a1bd4
datehoer <datehoer@gmail.com>
2024-11-06 20:57:10 +0800

    fix wallstreetcn

--- wallstreetcn/wallstreetcn.py
+++ wallstreetcn/wallstreetcn.py
@@ -1,23 +1,11 @@
-import requests
+from curl_cffi import requests
 import pyquery
 
 
 def get_wallstreetcn_data():
-    url = "https://wallstreetcn.com/"
+    url = "https://api-one-wscn.awtmt.com/apiv1/content/articles/hot?period=all"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
     }
-    data = []
     res = requests.get(url, headers=headers)
-    doc = pyquery.PyQuery(res.content)
-    ul = doc(".list.main .item").items()
-    for i in ul:
-        title = i.find("a").text()
-        link = i.find('a').attr('href')
-        data.append({
-            "url": link,
-            "title": title,
-            "hotScore": 0
-        })
-    return {"data": data}
-
+    return res.json()
