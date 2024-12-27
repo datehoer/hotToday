87804f59e91891054eb9e3ca35d0d034d66740ac
datehoer <62844803+datehoer@users.noreply.github.com>
2024-12-27 22:12:10 +0800

    update needknow

--- ithome/needknow.py
+++ ithome/needknow.py
@@ -4,8 +4,24 @@ import pyquery
 
 def get_ithome_needknow_data():
     url = "https://www.ithome.com/block/rank.html"
+    headers = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en,zh-CN;q=0.9,zh;q=0.8',
+        'cache-control': 'no-cache',
+        'pragma': 'no-cache',
+        'priority': 'u=0, i',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    }
 
-    res = requests.get(url)
+    res = requests.get(url, headers=headers)
     res.encoding = res.apparent_encoding
     doc = pyquery.PyQuery(res.text)
     data = []
