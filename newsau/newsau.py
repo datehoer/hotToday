92de3caf4f43be16460f2558fc38b2146caea14c
datehoer <datehoer@gmail.com>
2024-12-31 15:27:32 +0800

    fix error

--- newsau/newsau.py
+++ newsau/newsau.py
@@ -14,7 +14,6 @@ def get_newsau_data():
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
     }
     res = requests.get(url, headers=headers)
-    res.encoding = res.apparent_encoding
     doc = pyquery.PyQuery(res.text)
     items = doc("h4.storyblock_title").items()
     data = []
